14e294ee73802f06415f58b7536e07e1f89445cf
Jeremy Grecu <jeremy.grecu@sap.com>
2022-05-27 16:43:20 +0100

    refactor exception handling

--- src/main/java/antifraud/exceptions/RestExceptionHandler.java
+++ src/main/java/antifraud/exceptions/RestExceptionHandler.java
@@ -8,21 +8,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RestExceptionHandler {
 
     @ExceptionHandler
-    protected ResponseEntity<Object>  handleBadRequestException(BadRequestException ex) {
+    protected ResponseEntity<Object>  handleException(BadRequestException ex) {
         return ResponseEntity.badRequest().build();
     }
 
     @ExceptionHandler
-    protected ResponseEntity<Object> handleHttpConflictException(HttpConflictException ex) {
+    protected ResponseEntity<Object> handleException(HttpConflictException ex) {
         return ResponseEntity.status(409).build();
     }
     @ExceptionHandler
-    protected ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
+    protected ResponseEntity<Object> handleException(UserNotFoundException ex) {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler
-    protected ResponseEntity<Object> handleUserNotFoundException(UnprocessableEntityException ex) {
+    protected ResponseEntity<Object> handleException(UnprocessableEntityException ex) {
         return ResponseEntity.status(422).build();
     }
 
