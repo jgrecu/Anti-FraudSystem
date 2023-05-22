3b9ecbe14773a149424ec98ff24e592d5fc9733d
Jeremy Grecu <jeremy.grecu@sap.com>
2023-05-22 11:37:12 +0100

    Removed Unused Imports

--- src/main/java/antifraud/model/Card.java
+++ src/main/java/antifraud/model/Card.java
@@ -1,7 +1,5 @@
 package antifraud.model;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/antifraud/security/PasswordConfig.java
+++ src/main/java/antifraud/security/PasswordConfig.java
@@ -1,7 +1,6 @@
 package antifraud.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
